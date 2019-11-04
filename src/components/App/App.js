4c6bfdc020ad0faa598c969d50a6498398e46e58
Martha Sharpe <marthasharpe2020@gmail.com>
2019-11-04 13:32:32 -0500

    Fetch image and name from API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import PokemonImage from '../PokemonImage/PokemonImage';
 import PokemonName from '../PokemonName/PokemonName';
 import Letters from '../Letters/Letters';
 
 function App() {
+
+  const fetchPokemon = () => {
+    let randomNumber = Math.floor(Math.random() * 800);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.sprites.front_default);
+        console.log(data.species.name);
+      })
+    }
+
+  useEffect(() => {
+    fetchPokemon();
+  });
+
   return (
     <div className="App">
       <h1>Hang-Pokemon</h1>
